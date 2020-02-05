d726fb3f56777db819f45e8c8c3f127d2cd11b17
Nickita45 <supernikita45@gmail.com>
2020-02-05 15:26:13 +0200

    Added object OrderLine, ShaurmaType, Spice, Ingridients

---
+++ src/main/java/com/orders/management/domain/Ingridients.java
@@ -0,0 +1,34 @@
+package com.orders.management.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Ingridients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+}
