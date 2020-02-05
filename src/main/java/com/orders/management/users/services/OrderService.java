2f5047af81a57b6d3c9a7838464a41c65f3f549f
Vadym <strptvaxx@gmail.com>
2020-02-05 16:49:40 +0200

    Rewrited OrderService and implemented OrderServiceImplementation

--- src/main/java/com/orders/management/users/services/OrderService.java
+++ src/main/java/com/orders/management/users/services/OrderService.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public interface OrderService {
 
     int addOrder(Order order);
-    void deleteOrder(int id);
 
+    void deleteOrder(long id);
 }
--- src/main/java/com/orders/management/users/services/OrderServiceImplementation.java
+++ src/main/java/com/orders/management/users/services/OrderServiceImplementation.java
@@ -1,4 +1,22 @@
 package com.orders.management.users.services;
 
-public class OrderServiceImplementation {
+        import com.orders.management.domain.Order;
+        import com.orders.management.users.repository.OrderRepository;
+        import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImplementation implements OrderService{
+
+    private OrderRepository orderRepository;
+
+    @Override
+    public int addOrder(Order order) {
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Override
+    public void deleteOrder(long id) {
+        orderRepository.deleteById(id);
+    }
 }
