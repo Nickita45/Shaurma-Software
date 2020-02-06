2a97503fbbebc5424e2fbb4fb28938bb1fab1197
Alexey Petrov <alexeypetrov443@gmail.com>
2020-02-06 17:14:28 +0200

    .

---
+++ src/main/java/com/orders/management/domain/Role.java
@@ -0,0 +1,39 @@
+package com.orders.management.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role_table")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "employee_id")
+    private long employeeId;
+
+    @Column(name = "role_name",length = 30)
+    private String roleName;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void setRoleName(String roleName) { this.roleName = roleName; }
+    public String getRoleName() { return roleName;}
+
+    public void setId(long id){ this.id = id;}
+    public long getId(){ return id; }
+
+    public void setEmployeeIdId(long id){ this.employeeId = id;}
+    public long getEmployeeIdId(){ return employeeId; }
+
+    public void setUser(User user){ this.user=user; }
+    public User getUser(){ return user;}
+
+}
--- src/main/java/com/orders/management/domain/User.java
+++ src/main/java/com/orders/management/domain/User.java
@@ -1,37 +1,56 @@
 package com.orders.management.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
 
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
+    @Column(name = "user_id")
+    private long userId;
+    @Column(name = "first_name",length = 40)
     private String firstName;
+    @Column(name = "last_name",length = 40)
     private String lastName;
+    @Column(name = "login",length = 40)
+    private String login;
+    @Column(name = "password",length = 40)
+    private String password;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "email",length = 40)
+    private String email;
 
-    public String getFirstName() {
-        return firstName;
-    }
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "user",fetch = FetchType.EAGER)
+    private Set<Role> roleList;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setId(long id){this.userId = id;}
+    public long getId(){return userId; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getFirstName() { return firstName; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) {this.lastName = lastName; }
+    public String getLastName() { return lastName; }
 
-    public long getId(){
-        return id;
-    }
+    public void setLogin(String login){ this.login = login; }
+    public  String getLogin(){ return login; }
+
+    public void setPassword(String password){ this.password = password; }
+    public String getPassword(){ return password; }
+
+    public void setActive(boolean active){ this.active = active; }
+    public boolean getActive(){ return active; }
+
+    public void setEmail(String email){ this.email = email; }
+    public String getEmail(){ return email;}
+
+    public void setRoleList(Set<Role> roleList){ this.roleList=roleList; }
+    public Set<Role> getRoleList(){ return roleList; }
+
 }
