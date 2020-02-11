1cf514d6e38b07eea6fe0447743e0321ba8b474d
YourVadymstop <strptvaxx@gmail.com>
2020-02-11 15:16:35 +0200

    Final

--- src/main/resources/db/migration/V2__test_files.sql
+++ src/main/resources/db/migration/V2__test_files.sql
@@ -1,3 +1,5 @@
-create table document (id integer not null, date datetime(6), done bit not null, user_id integer not null, primary key (id)) engine=InnoDB
-create table hibernate_sequences (sequence_name varchar(255) not null, next_val bigint, primary key (sequence_name)) engine=InnoDB
-insert into hibernate_sequences(sequence_name, next_val) values ('default',0)
+drop table if exists hibernate_sequence;
+drop table if exists document;
+create table hibernate_sequence (next_val bigint);
+insert into hibernate_sequence values ( 1 );
+create table document (id integer not null AUTO_INCREMENT, date datetime(6), done bit not null, user_id integer not null, primary key (id)) engine=InnoDB
